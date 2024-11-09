42c4a1a81a2d16665fd1a90760ef9a6378703c8b
Farhath <132221486+abdulfarhath@users.noreply.github.com>
2024-11-09 17:01:14 +0530

    discussion page using arrays
    basic discussions page created
    needs design and
    backend for storage and fetching

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const doubtsRouter = require('./doubts'); // Import the doubts route module
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Use the doubts router under the '/doubts' path
+app.use('/doubts', doubtsRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
