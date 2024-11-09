c7533d309d25f8468be3c4de01d8d91c312b272c
AnirudhRakonda <140954132+AnirudhRakonda@users.noreply.github.com>
2024-11-09 17:23:47 +0530

    made signin pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
 
-import Dashboard from './pages/Dashboard';
-import VideoChat from './pages/VideoChat';
-import Resources from './pages/Resources';
-import Messages from './pages/Messages';
-import Groups from './pages/Groups';
-import Channels from './pages/Channels';
-import Discussion from './pages/Discussions';
-import Quiz from './pages/Quiz';
-import Feed from './pages/Feed';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 import Landing from './pages/Landing';
 
 function App() {
