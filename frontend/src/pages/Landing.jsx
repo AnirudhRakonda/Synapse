a2fd56b38d3d8d0d71e709cac7950e8de9311adc
SiddharthaArepalli <asiddharthagoud@gmail.com>
2024-11-09 22:49:19 +0530

    changes the ui landing
    changes the ui landing

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,14 +4,14 @@ const Landing = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-primary text-textColor">
     <header className="text-center mb-8">
-      <h1 className="text-8xl font-bold text-cta mb-4">Welcome to SYNAPSE</h1>
+      <h1 className="text-8xl font-bold text-cta mb-4">Welcome to <span className='text-white'>Synapse</span></h1>
       <p className="text-[18px] text-textColor max-w-lg mx-auto">
         Join our interactive platform for university students to learn, teach, and grow together. Whether you're
         mastering complex subjects or helping others, PeerLearn offers the tools to collaborate, support, and succeed.
       </p>
     </header>
 
-    <div className="flex flex-col items-center justify-center space-y-4">
+    <div className="flex flex-row items-center justify-center space-x-4">
       <Link
         to="/signin"
         className="px-8 py-3 bg-cta text-white rounded-lg font-semibold text-lg hover:bg-active transition-colors"
